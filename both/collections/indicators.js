55df4010c9404c29fb370ec8d9b05fed5e2441ac
slayug <bug.Graph@gmail.com>
2016-03-05 15:26:24 +0100

    update category's indicator OK

--- both/collections/indicators.js
+++ both/collections/indicators.js
@@ -1,5 +1,9 @@
 Indicators = new Mongo.Collection('indicators');
 
 Indicators.helpers({
-
 });
+Indicators.allow({
+	update:function(){
+		return true;
+	}
+})
--- client/templates/manageIndicator/manageIndicator.js
+++ client/templates/manageIndicator/manageIndicator.js
@@ -1,7 +1,18 @@
 Template.manageIndicator.helpers({
 	indicators: function(){
 		var ind = Indicators.find().fetch();
+		console.log(ind);
 		return ind;
+	}
+});
+Template.indicator.helpers({
+	categories:function(){
+		return Categories.find();
+	}
+});
+Template.category.helpers({
+	equals: function(a, b){
+		return a === b;
 	}
 });
 Template.manageIndicator.events({
@@ -20,14 +31,20 @@ Template.manageIndicator.events({
 		var text = event.target.text.value;
 		//TODO test si y'a plusieurs lignes de lien si c'est le cas appelé plusieurs fois importIndicator
 		Meteor.call('importIndicator', text);
+	},
+	"click .cat":function(){
+		Meteor.call('createCategories');
 	}
 });
 
 Template.indicator.events({
-    "click .delete": function( ){
-        Meteor.call( 'deleteIndicator', this._id );
-    },
-	"click .cat":function(){
-		Meteor.call('createCategories');
+	"click .delete": function( ){
+		Meteor.call( 'deleteIndicator', this._id );
+	},
+	'change .select-category': function(e){
+		var idCategory = $(event.target).find('option:selected').val();
+		Indicators.update(this._id, {
+			$set: {id_category: idCategory}
+		});
 	}
 })

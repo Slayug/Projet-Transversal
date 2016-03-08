241cc230a4b2c12ca83ec3b786744682326644c9
cyhors <cyhors@gmail.com>
2016-03-08 17:25:39 +0100

    Remove Countries.insert from first indice

--- server/publications/countries.js
+++ server/publications/countries.js
@@ -1,15 +1,3 @@
 Meteor.publish('countries', function(){
-
-    //If there's nothing in the DB
-    if( Countries.find().count( ) <= 0 ){
-        //Get the first indicator
-        var indicators = Indicators.find().fetch()[0];
-        //Broswe through all countries code.
-        Object.keys( indicators.countries ).forEach( function( codeCountrie ){
-            //Insert the the countrie info into Countries
-            Countries.insert( { name: indicators.countries[ codeCountrie ].name, code: codeCountrie } );
-        });
-    }
-
 	return Countries.find({}, {limit:250});
 })

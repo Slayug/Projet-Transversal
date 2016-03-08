7d66c0ff78931122c06b9195aff421c10cc6fda2
cyhors <cyhors@gmail.com>
2016-03-08 10:01:17 +0100

    Add checkbox for indices. The check will update the graph.

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -11,3 +11,25 @@ Template.homeCategory.helpers({
 		return Indicators.find( { id_category: { $in: [idCategory]} } );
 	}
 });
+
+Template.homeIndicator.events({
+    "click .checkIndicator": function( ){
+        //Get the indices. It's an array
+        var indices = Session.get( "indices" );
+
+        //If the indice is already checked
+        if( indices.includes( this.code ) ){
+            //Remove the indice from the indices
+            indices.splice( indices.indexOf( this.code ), 1 );
+            //Update the session
+            Session.set( "indices", indices );
+        }
+        //If the indice have been just checked
+        else{
+            //Add the indice to the indices
+            indices.push( this.code );
+            //Update the session
+            Session.set( "indices", indices );
+        }
+    }
+});

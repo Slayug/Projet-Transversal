429889fd1b7bcfcd59901398e92de4531bf8d73c
cyhors <cyhors@gmail.com>
2016-03-29 23:31:52 +0200

    Mini fixes

--- both/router/routes.js
+++ both/router/routes.js
@@ -7,6 +7,7 @@ Router.route('/', {
       Session.set( "indicators", [ "SP.POP.TOTL" ] );
       Session.set( "typeChoice", "evolution" );
       Session.set( "indicatorSelected", "SP.POP.TOTL" );
+      Session.setDefault( "countriesClassement", [] );
       return [];
   },
   subscriptions: function(){
